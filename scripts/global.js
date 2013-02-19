f57d4c8fc692ae923cb1d3d6321e52dc570628b0
Trustin Lee <trustin@gmail.com>
2013-02-18 22:50:52 -0800

    Fix the search form in 404

--- scripts/global.js
+++ scripts/global.js
@@ -3,3 +3,8 @@ function validateGlobalSearchQuery() {
   return gsq && gsq.value.length > 0;
 }
 
+function validate404SearchQuery() {
+  var gsq = document.getElementById('404-search-query');
+  return gsq && gsq.value.length > 0;
+}
+
