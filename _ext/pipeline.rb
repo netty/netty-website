a07daef8c470952dab6eda6ccbcfe2ae50ae2fec
Trustin Lee <trustin@gmail.com>
2013-02-19 14:28:39 -0800

    Add news feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,7 @@
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news', :posts )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/news/index.atom' )
   extension Awestruct::Extensions::Disqus.new()
 end
 
