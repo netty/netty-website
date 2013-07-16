193627618f5dc7431d4e4e338d728fa5244ebb99
Trustin Lee <t@motd.kr>
2013-07-16 11:50:23 +0900

    Improve error reporting of update-wiki.sh

--- _bin/update-wiki.sh
+++ _bin/update-wiki.sh
@@ -31,6 +31,8 @@ cat 'wiki/_pages' | while read -r LINE; do
     fi
     WIKI_TITLE=${BASH_REMATCH[4]}
     echo Retrieving: "$WIKI_FILE" from "$WIKI_URI" ..
+    WIKI_TMPFILE="`mktemp`"
+
     {
       echo '---'
       echo 'layout: wiki'
@@ -40,81 +42,83 @@ cat 'wiki/_pages' | while read -r LINE; do
       echo '---'
       echo
       echo ':plain'
-      {
-        echo '# encoding: UTF-8'
-        echo "require 'nokogiri'"
-        echo "require 'htmlentities'"
-        echo "@doc = Nokogiri::HTML::DocumentFragment.parse <<-'EOF_92ca82985abd11f6a579fe9b19b578020e0d454d'"
-        curl -s "https://github.com$WIKI_URI" || exit 1
-        echo
-        echo 'EOF_92ca82985abd11f6a579fe9b19b578020e0d454d'
+    } > "$WIKI_TMPFILE"
 
-        # Generate TOC
-        echo '
-          headings = @doc.css("h2,h3,h4")
-          if headings.size() > 1 and @doc.css("#wiki-notoc").size() == 0
-            coder = HTMLEntities.new
-            toc_idx = 0
-            toc_level = 2
-            first = true
-            puts "<div class=\"wiki-toc well pull-right\">"
-            puts "<ul class=\"nav nav-list\">"
-            puts "<li class=\"nav-header\">Table of Contents"
-            for h in headings
-              section_id = "wiki-" + h.name + "-" + toc_idx.to_s
-              toc_idx = toc_idx + 1
-              h["id"] = section_id
-              new_toc_level = h.name[1].ord - 48
-              toc_text = coder.encode(h.inner_text)
-              if new_toc_level == toc_level
-                puts "</li><li><a href=\"#" + section_id + "\" title=\"" + toc_text + "\">" + toc_text + "</a>"
-                first = false
-              elsif new_toc_level == toc_level + 1
-                if first # first heading is not h2 but h3
-                  puts "</li><li>"
-                end
-                toc_level = new_toc_level
-                puts "<ul class=\"nav nav-list\"><li><a href=\"#" + section_id + "\" title=\"" + toc_text + "\">" + toc_text + "</a>"
-                first = false;
-              elsif !first and new_toc_level == toc_level - 1
-                toc_level = new_toc_level
-                puts "</li></ul></li><li><a href=\"#" + section_id + "\" title=\"" + toc_text + "\">" + toc_text + "</a>"
-                first = false;
-              end
-            end
-            while toc_level >= 2
-              toc_level = toc_level - 1
-              puts "</li></ul>"
-            end
-            puts "</div>"
-          end
-        '
-
-        # Enable tab switching
-        echo '
-          for e in @doc.css("ul.nav-tabs li a")
-            e["data-toggle"] = "tab"
-          end
-        '
-
-        echo 'print @doc.at_css "div#wiki-body"'
-        # Perl regex soup below:
-        # 1. DOS to UNIX
-        # 2. Indentation
-        # 3. Remove broken links
-        # 4. Make internal links to wiki home work
-        # 5. Make internal links to other wiki pages work
-        # 6. Add Bootstrap class to table tags
-      } | ruby \
-        | perl -p0 -e 's/[\r]//g' \
-        | perl -p -e 's/^/  /g' \
-        | perl -p -e 's/<a [^>]*absent[^>]*>(((?!<\/a>).)*)<\/a>/<span class="broken-link">$1<\/span>/gi' \
-        | perl -p -e 's#/netty/netty/wiki/(Home)?"#index.html"#gi' \
-        | perl -p -e 's#/netty/netty/wiki/((?!images/)[^"]+)#\L$1.html#g' \
-        | perl -p -e 's/<table>/<table class="table table-hover">/gi' \
-        || exit 1
+    {
+      echo '# encoding: UTF-8'
+      echo "require 'nokogiri'"
+      echo "require 'htmlentities'"
+      echo "@doc = Nokogiri::HTML::DocumentFragment.parse <<-'EOF_92ca82985abd11f6a579fe9b19b578020e0d454d'"
+      curl -s "https://github.com$WIKI_URI" || exit 1
       echo
-    } > "wiki/$WIKI_FILE.html.haml"
+      echo 'EOF_92ca82985abd11f6a579fe9b19b578020e0d454d'
+
+      # Generate TOC
+      echo '
+        headings = @doc.css("h2,h3,h4")
+        if headings.size() > 1 and @doc.css("#wiki-notoc").size() == 0
+          coder = HTMLEntities.new
+          toc_idx = 0
+          toc_level = 2
+          first = true
+          puts "<div class=\"wiki-toc well pull-right\">"
+          puts "<ul class=\"nav nav-list\">"
+          puts "<li class=\"nav-header\">Table of Contents"
+          for h in headings
+            section_id = "wiki-" + h.name + "-" + toc_idx.to_s
+            toc_idx = toc_idx + 1
+            h["id"] = section_id
+            new_toc_level = h.name[1].ord - 48
+            toc_text = coder.encode(h.inner_text)
+            if new_toc_level == toc_level
+              puts "</li><li><a href=\"#" + section_id + "\" title=\"" + toc_text + "\">" + toc_text + "</a>"
+              first = false
+            elsif new_toc_level == toc_level + 1
+              if first # first heading is not h2 but h3
+                puts "</li><li>"
+              end
+              toc_level = new_toc_level
+              puts "<ul class=\"nav nav-list\"><li><a href=\"#" + section_id + "\" title=\"" + toc_text + "\">" + toc_text + "</a>"
+              first = false;
+            elsif !first and new_toc_level == toc_level - 1
+              toc_level = new_toc_level
+              puts "</li></ul></li><li><a href=\"#" + section_id + "\" title=\"" + toc_text + "\">" + toc_text + "</a>"
+              first = false;
+            end
+          end
+          while toc_level >= 2
+            toc_level = toc_level - 1
+            puts "</li></ul>"
+          end
+          puts "</div>"
+        end
+      '
+
+      # Enable tab switching
+      echo '
+        for e in @doc.css("ul.nav-tabs li a")
+          e["data-toggle"] = "tab"
+        end
+      '
+
+      echo 'print @doc.at_css "div#wiki-body"'
+    } | ruby >> "$WIKI_TMPFILE"
+      
+    # Perl regex soup below:
+    # 1. DOS to UNIX
+    # 2. Indentation
+    # 3. Remove broken links
+    # 4. Make internal links to wiki home work
+    # 5. Make internal links to other wiki pages work
+    # 6. Add Bootstrap class to table tags
+    perl -pi0 -e 's/[\r]//g' "$WIKI_TMPFILE"
+    perl -pi -e 's/^/  /g' "$WIKI_TMPFILE"
+    perl -pi -e 's/<a [^>]*absent[^>]*>(((?!<\/a>).)*)<\/a>/<span class="broken-link">$1<\/span>/gi' "$WIKI_TMPFILE"
+    perl -pi -e 's#/netty/netty/wiki/(Home)?"#index.html"#gi' "$WIKI_TMPFILE"
+    perl -pi -e 's#/netty/netty/wiki/((?!images/)[^"]+)#\L$1.html#g' "$WIKI_TMPFILE"
+    perl -pi -e 's/<table>/<table class="table table-hover">/gi' "$WIKI_TMPFILE"
+
+    mv -f "$WIKI_TMPFILE" "wiki/$WIKI_FILE.html.haml"
 
     {
       echo '  %li'
