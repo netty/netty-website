bc2fde632fb4ee298d34644b6a1f1275076edf12
Trustin Lee <t@motd.kr>
2013-11-07 19:31:27 +0900

    Improve the deployment script so that the history of the deployment repository doesn't grow infinitely

--- _bin/deploy.sh
+++ _bin/deploy.sh
@@ -53,21 +53,49 @@ pushd "$BIN/.."
 bundle exec awestruct --generate --force || exit 1
 cp -R 3.* 4.* "$SRC" || exit 1
 popd
+touch "$SRC/.nojekyll"
 
 # Pull the latest changes in netty.github.com
 pushd "$DST"
-git pull --ff-only
+git reset --hard HEAD || exit 1
+git checkout master || exit 1
+git pull --ff-only origin master || exit 1
+NUM_DEPLOYS=$(git rev-list --count HEAD)
 popd
+
+if [[ -z "$NUM_DEPLOYS" ]]; then
+  echo 'Failed to get the repository information.'
+  exit 1
+fi
 
 # Inject Google Analytics JavaScript in all generated HTMLs
 "$BIN/inject-google-analytics.sh" || exit 1
 
-# Copy the generated web site to netty.github.com
-rsync -a "$SRC/" "$DST" || exit 1
+DATE="$(date -u '+%Y-%m-%d %H:%M:%S')"
+if [[ $NUM_DEPLOYS -lt 16 ]]; then
+  # Copy the generated web site to netty.github.com
+  rsync -a --delete --exclude=.git/ "$SRC/" "$DST" || exit 1
 
-# Publish the generated web site
-cd "$DST"
-git add .
-git commit -m "Deploy site '$(git log -1 --format=format:%h)' on $(date)"
-git push
+  # Publish the generated web site
+  cd "$DST"
+  git add -A
+  git commit -m "Deploy site '$(git log -1 --format=format:%h)' on $DATE"
+  git push -u --force origin master
+else
+  # Rewind to the initial commit.
+  pushd "$DST"
+  git checkout 533f5da7361390c306889bbe6bfe25a47b2f4a9c || exit 1
+  git branch -D master || exit 1
+  git checkout -B master || exit 1
+  popd
+
+  # Copy the generated web site to netty.github.com
+  rsync -a --delete --exclude=.git/ "$SRC/" "$DST" || exit 1
+
+  # Publish the generated web site
+  cd "$DST"
+  git add -A
+  git commit -m "Deploy site '$(git log -1 --format=format:%h)' on $DATE"
+  git push -u --force origin master
+fi
 
