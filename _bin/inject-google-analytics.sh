39995fb6a195f3b5dc89c6c8de08a47a7ec21918
Trustin Lee <t@motd.kr>
2013-12-15 15:24:44 +0900

    Improve the performance of inject-google-analytics.sh

--- _bin/inject-google-analytics.sh
+++ _bin/inject-google-analytics.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
+
+cd "`dirname "$0"`/../_site"
 
 TRACKER_ID="UA-95307-5"
-
 TRACKER_CODE="<script type=\"text/javascript\">
 var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
 document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
@@ -12,30 +13,19 @@ var pageTracker = _gat._getTracker('$TRACKER_ID');
 pageTracker._trackPageview();
 } catch(err) {}</script>"
 
-cd "`dirname $0`/../_site"
-if [[ "$?" != '0' ]]; then
-  echo "Failed to find the base directory."
-  exit 1
+# Find the files injected already.
+grep -Flr "$(printf "<frameset\n<FRAMESET\n$TRACKER_ID")" . | sort > ../.analytics-injected
+
+# Find the files not injected yet.
+find . '(' -name '*.html' -and -not -name '*-frame.html' ')' | sort | comm -23 - ../.analytics-injected > ../.analytics-uninjected
+
+# Inject the files.
+NUM_FILES=`wc -l < ../.analytics-uninjected`
+if [[ $NUM_FILES -gt 0 ]]; then
+  echo -n "Injecting $NUM_FILES file(s) .. "
+  cat ../.analytics-uninjected | tr '\n' '\0' | xargs -0 -n 32 perl -pi -e "s/(<\\/body>)/\\n${TRACKER_CODE////\\/}\\n\$1/i"
+  echo done
+else
+  echo "No files to inject."
 fi
-
-find . -name '*.html.new' -delete
-find . '(' -name '*.html' -and -not -name '*-frame.html' ')' | while read -r TARGET; do
-  if grep -qiE "(<frameset|$TRACKER_ID)" "$TARGET"; then
-    continue;
-  fi
-
-  if grep -qiF '</body>' "$TARGET"; then
-    perl -p -e "s/(<\\/body>)/\\n${TRACKER_CODE////\\/}\\n\$1/i" < "$TARGET" > "$TARGET.new"
-  else
-    { cat "$TARGET"; echo; echo "$TRACKER_CODE"; echo; } > "$TARGET.new"
-  fi
-  if ! grep -qF "$TRACKER_ID" "$TARGET.new"; then
-    echo "Failed to inject analytics script: $TARGET"
-    cat "$TARGET.new"
-    rm -f "$TARGET.new"
-    exit 1
-  fi
-  mv -f "$TARGET.new" "$TARGET"
-  echo "$TARGET"
-done
 
