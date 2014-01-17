90170cf9f9ca84318c2a442af1de5989a99a7879
Trustin Lee <t@motd.kr>
2014-01-17 13:26:54 +0900

    Generate the example list dynamically
    .. so we don't need to update the hyperlinks anymore

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -21,19 +21,66 @@ end
 
 class ReleaseInfo
   def execute(site)
+    simple_versions = Hash.new
     for r in site.releases
+      r.site = site
+      r.extend Release
+
+      if r.stable and site.latest_stable_release.nil?
+        site.latest_stable_release = r
+      end
+
+      if r.major_version.nil?
+        r.major_version = r.version.sub(/^([0-9]+).*$/, '\1').to_i
+      end
       if r.simple_version.nil?
         r.simple_version = r.version.sub(/^([0-9]+\.[0-9]+).*$/, '\1')
       end
+      if not simple_versions.has_key?(r.simple_version)
+        simple_versions[r.simple_version] = r
+      end
       if r.filename.nil?
-        if r.version.start_with?("3.")
-          r.filename = 'netty-' + r.version + '-dist.tar.bz2'
-        else
+        if r.major_version > 3
           r.filename = 'netty-' + r.version + '.tar.bz2'
+        else
+          r.filename = 'netty-' + r.version + '-dist.tar.bz2'
         end
       end
       if r.branch.nil?
         r.branch = r.simple_version
+      end
+#      if r.example_url.nil?
+#        if r.major_version > 3
+#          r.example_url = site.base_url + r.simple_version + '/xref/io/netty/example'
+#        else
+#          r.example_url = site.base_url + r.simple_version + '/xref/io/netty/example'
+#          r.example_url = 'https://github.com/netty/netty/tree/' + r.branch + '/src/main/java/org/jboss/netty/example'
+#        end
+#      end
+    end
+
+    site.major_releases = simple_versions.values
+  end
+
+  module Release
+    def example_url(name = nil)
+      if self.major_version > 3
+        if name.nil?
+          'https://github.com/netty/netty/tree/' + self.branch + '/example/src/main/java/io/netty/example'
+        else
+          site.base_url + '/' + self.simple_version + '/xref/io/netty/example/' + name + '/package-summary.html'
+        end
+      else
+        # The WorldClock example was LocalTime in 3.
+        if name == 'worldclock'
+          name = 'localtime'
+        end
+
+        if name.nil?
+          'https://github.com/netty/netty/tree/' + self.branch + '/src/main/java/org/jboss/netty/example'
+        else
+          site.base_url + '/' + self.simple_version + '/xref/org/jboss/netty/example/' + name + '/package-summary.html'
+        end
       end
     end
   end
