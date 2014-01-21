5a882f4cbe2d46a7d72777dbaf71a84d4722cd38
Trustin Lee <t@motd.kr>
2014-01-21 17:11:36 +0900

    Allow specifying arguments in inject-google-analytics.sh

--- _bin/inject-google-analytics.sh
+++ _bin/inject-google-analytics.sh
@@ -1,6 +1,19 @@
 #!/bin/bash -e
 
-cd "`dirname "$0"`/../_site"
+BASE_DIR="`dirname "$0"`/.."
+BASE_DIR="`readlink -m "$BASE_DIR"`"
+
+if [[ $# -eq 0 ]]; then
+  cd "$BASE_DIR/_site"
+elif [[ $# -eq 1 ]]; then
+  cd "$1"
+else
+  while [[ $# -gt 1 ]]; do
+    "$0" "$1"
+    shift
+  done
+  cd "$1"
+fi
 
 TRACKER_ID="UA-95307-5"
 TRACKER_CODE="<script type=\"text/javascript\">
@@ -14,16 +27,16 @@ pageTracker._trackPageview();
 } catch(err) {}</script>"
 
 # Find the files injected already.
-grep -Flr "$(printf "<frameset\n<FRAMESET\n$TRACKER_ID")" . | sort > ../.analytics-injected
+grep -Flr "$(printf "<frameset\n<FRAMESET\n$TRACKER_ID")" . | sort > "$BASE_DIR/.analytics-injected"
 
 # Find the files not injected yet.
-find . '(' -name '*.html' -and -not -name '*-frame.html' ')' | sort | comm -23 - ../.analytics-injected > ../.analytics-uninjected
+find . '(' -name '*.html' -and -not -name '*-frame.html' ')' | sort | comm -23 - "$BASE_DIR/.analytics-injected" > "$BASE_DIR/.analytics-uninjected"
 
 # Inject the files.
-NUM_FILES=`wc -l < ../.analytics-uninjected`
+NUM_FILES=`wc -l < "$BASE_DIR/.analytics-uninjected"`
 if [[ $NUM_FILES -gt 0 ]]; then
   echo -n "Injecting $NUM_FILES file(s) .. "
-  cat ../.analytics-uninjected | tr '\n' '\0' | xargs -0 -n 32 perl -pi -e "s/(<\\/body>)/\\n${TRACKER_CODE////\\/}\\n\$1/i"
+  cat "$BASE_DIR/.analytics-uninjected" | tr '\n' '\0' | xargs -0 -n 32 perl -pi -e "s/(<\\/body>)/\\n${TRACKER_CODE////\\/}\\n\$1/i"
   echo done
 else
   echo "No files to inject."
