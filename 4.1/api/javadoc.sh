d1aa3a401148dd694f68669f14bcbe97f1e3a48d
Norman Maurer <norman_maurer@apple.com>
2017-08-25 12:48:30 +0200

    New netty releases

--- 4.1/api/javadoc.sh
+++ 4.1/api/javadoc.sh
@@ -1,1 +1,1 @@
-/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.141-2.b16.el6_9.x86_64/jre/../bin/javadoc @options @packages
+/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.121-0.b13.el6_8.x86_64/jre/../bin/javadoc @options @packages
