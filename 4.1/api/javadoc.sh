61c22066fd1ce8764e3dcabda88d84279e597390
Norman Maurer <norman_maurer@apple.com>
2019-09-12 14:26:42 +0200

    Prepare for next release

--- 4.1/api/javadoc.sh
+++ 4.1/api/javadoc.sh
@@ -1,1 +1,1 @@
-/Library/Java/JavaVirtualMachines/applejdk-8.jdk/Contents/Home/jre/../bin/javadoc @options @packages
+/Users/norman/.jabba/jdk/adopt@1.8.212-04/Contents/Home/jre/../bin/javadoc @options @packages
